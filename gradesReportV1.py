d4ec839c0c704d9b4b34496653d8c4ddf812d9be
aamehri <aamehri@loyola.edu>
2021-11-22 08:31:39 -0500

    cm02

--- gradesReportV1.py
+++ gradesReportV1.py
@@ -1,8 +1,15 @@
 # Extra Assignment for CS151
+def add_student(stu_data):
+    print("Student Utility")
+    student_ID = input("Enter a unique student ID: ")
+    student_name = input("Enter the student name: ")
+    stu_data[student_ID] = [student_name, 0.0, 0.0, 0.0]
+
 
 def menu():
     print("Utility Menu\n"
           "============\n"
+          "0. Load students from file\n"
           "1. Add Students\n"
           "2. Add grades\n"
           "Report Menu\n"
@@ -13,7 +20,12 @@ def menu():
     return choice
 
 def main():
+    students = {} # key student ID, names and grades will be payload
     option = '0'
     while option != '4':
         option = menu()
+        if option == '1':
+            add_student(students)
+
+    print(students)
 main()
