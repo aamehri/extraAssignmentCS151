dad34bfc1071389e9f72ebe7451f54a80b556052
aamehri <aamehri@loyola.edu>
2021-11-22 08:01:53 -0500

    cm01

--- gradesReportV1.py
+++ gradesReportV1.py
@@ -1,7 +1,19 @@
 # Extra Assignment for CS151
 
-
+def menu():
+    print("Utility Menu\n"
+          "============\n"
+          "1. Add Students\n"
+          "2. Add grades\n"
+          "Report Menu\n"
+          "===========\n"
+          "3. Run Report\n"
+          "4. EXIT")
+    choice = input("Enter your choice: ")
+    return choice
 
 def main():
-    pass
+    option = '0'
+    while option != '4':
+        option = menu()
 main()
